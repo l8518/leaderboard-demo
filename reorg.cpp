d05ba0ea12e8b85dd6fe0b2c123044126abf0640
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-09-13 12:22:29 +0200

    add location bin file

--- reorg.cpp
+++ reorg.cpp
@@ -6,13 +6,43 @@
 
 #include "utils.h"
 
+unsigned long person_length, knows_length, interest_length;
+Person *person_map;
+FILE *outfile;
+
+typedef struct {
+	unsigned short location;
+} LocationBin;
+
+/**
+ * Prepare a bin file, that only contains the location and the person offset (implicitly)
+ * */
+void prepare_person_location_bin() {
+
+	unsigned int i, max_i;
+	max_i = person_length / sizeof(Person);
+	Person *p;
+	LocationBin* results = (LocationBin*)malloc(max_i * sizeof (LocationBin));
+
+	for (i = 0; i < max_i; i++)
+		results[i].location = (&person_map[i])->location;
+
+	outfile = fopen("person_location.bin", "w");
+	// output
+	for (i = 0; i < max_i; i++)
+		fprintf(outfile, "%hu\n", results[i].location);
+
+	return;
+}
+
 int main(int argc, char *argv[]) {
 	char* person_output_file   = makepath((char*)argv[1], (char*)"person",   (char*)"bin");
 	char* interest_output_file = makepath((char*)argv[1], (char*)"interest", (char*)"bin");
 	char* knows_output_file    = makepath((char*)argv[1], (char*)"knows",    (char*)"bin");
 	
+	person_map = (Person *)mmapr(person_output_file, &person_length);
 	// this does not do anything yet. But it could...
-	
+	prepare_person_location_bin();
 	return 0;
 }
 
