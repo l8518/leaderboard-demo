270a7da0de9d7c4211d8f0d124fa6888ad1f0533
Leonard Herold <35725355+l8518@users.noreply.github.com>
2019-09-13 23:22:14 +0200

    add zero friends filter

--- reorg.cpp
+++ reorg.cpp
@@ -6,6 +6,7 @@
 
 #include "utils.h"
 #include <chrono>
+#include <map>
 
 // Variables:
 unsigned long person_length, knows_length, interest_length;
@@ -56,6 +57,57 @@ void filter_person_location(FILE *knows_out, FILE *person_out)
 	}
 }
 
+void filter_person_no_friends(FILE *knows_out, FILE *person_out) {
+	unsigned int i, max_i;
+	unsigned int j, max_j;
+	unsigned int new_i = 0;
+	unsigned int new_knows_pos = 0;
+	max_i = person_length / sizeof(Person);
+	std::map<int, unsigned int> keeper;
+	Person *p, *k;
+	Person *new_p = new Person();
+	
+	// determine all people to keep.
+	for (i = 0; i < max_i; i++) {
+		p = &person_map[i];
+		if (p->knows_n == 0) continue;
+		keeper[i] = new_i;
+		new_i++;
+	}
+
+	for (i = 0; i < max_i; i++)
+	{
+		if (keeper.find(i) == keeper.end()) continue;
+
+		p = &person_map[i];
+		max_j = p->knows_first + p->knows_n;
+
+		unsigned int start_new_knows_pos = new_knows_pos;
+		// iterate over all friendships
+		for (j = p->knows_first; j < max_j; j++)
+		{
+			unsigned int offset = knows_map[j];
+			if (keeper.find(offset) == keeper.end()) continue;
+			unsigned int newOff= keeper[offset];
+			fwrite(&newOff, sizeof(unsigned int), 1, knows_out);
+			new_knows_pos++;
+		}
+
+		// Write Binary Version:
+		new_p->person_id = (unsigned long)p->person_id;
+		new_p->birthday = (unsigned short)p->birthday;
+		new_p->location = (unsigned short)p->location;
+		new_p->knows_first = (unsigned long)start_new_knows_pos;
+		new_p->knows_n = (unsigned short)(new_knows_pos - start_new_knows_pos);
+		new_p->interests_first = (unsigned long)p->interests_first;
+		new_p->interest_n = (unsigned short)p->interest_n;
+
+		// write binary person record to file
+		fwrite(new_p, sizeof(Person), 1, person_out);
+	}
+
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -70,14 +122,32 @@ int main(int argc, char *argv[])
 	person_map = (Person *)mmapr(person_output_file, &person_length);
 	knows_map = (unsigned int *)mmapr(knows_output_file, &knows_length);
 
-	// New Output files
+	// STEP 01: Filter for locality
 	char *person_location_output_file = makepath(folder, (char *)"location_person", (char *)"bin");
 	char *knows_location_output_file = makepath(folder, (char *)"location_knows", (char *)"bin");
-	// MMAP Output files
 	FILE *knows_out = open_binout(knows_location_output_file);
 	FILE *person_out = open_binout(person_location_output_file);;
-
 	filter_person_location(knows_out, person_out);
+	fclose(person_out);
+	fclose(knows_out);
+
+	// Unmap previous person_map and remap to processed one:
+	printf("%d \n", knows_length);
+	munmap(person_map, person_length);
+	munmap(knows_map, knows_length);
+	person_map = (Person *)mmapr(person_location_output_file, &person_length);
+	knows_map = (unsigned int *)mmapr(knows_location_output_file, &knows_length);
+
+	printf("%d \n", knows_length);
+	// STEP 02: Filter for no friends (as they will not be considerd - only mutal friends)
+	char *person_location_friends_output_file = makepath(folder, (char *)"location_friends_person", (char *)"bin");
+	char *knows_location_friends_output_file = makepath(folder, (char *)"location_friends_knows", (char *)"bin");
+	person_out = open_binout(person_location_friends_output_file);
+	knows_out = open_binout(knows_location_friends_output_file);;
+	filter_person_no_friends(knows_out, person_out);
+	fclose(person_out);
+	fclose(knows_out);
+
 
 	printf("Starting reorg \n");
 	//take time:
